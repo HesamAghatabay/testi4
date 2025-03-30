638c75914955a1b85d8d2ca8f2bac3fc05b5b123
Hesam Aghatabay <program.h.p.2023@gmail.com>
2025-03-30 14:40:59 +0330

    add new pages

---
+++ BE/app/Http/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            "name" => $request->name,
+            "mobile" => $request->mobile,
+            "email" => $request->email,
+            "password" => Hash::make($request->password),
+        ]);
+        if (!$user) {
+            return response()->json([
+                "status" => "error",
+                "message" => "register wring!"
+            ], 400);
+        }
+        return response()->json([
+            "status" => "success",
+            "message" => "register user successfully",
+        ], 201);
+    }
+}
