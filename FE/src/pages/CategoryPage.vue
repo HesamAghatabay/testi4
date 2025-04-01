e4658ad77dab0cc04a2281f47e6103f69145211e
Hesam Aghatabay <program.h.p.2023@gmail.com>
2025-04-01 23:57:40 +0330

    update

--- FE/src/pages/CategoryPage.vue
+++ FE/src/pages/CategoryPage.vue
@@ -1,11 +1,37 @@
 <template>
   <q-page padding>
     <!-- content -->
-    <q-btn to="create-category" color="green-9">Create New Category</q-btn>
-  
+    <q-btn to="create-category" class="q-my-md" color="green-9">Create New Category</q-btn>
+
+    <div class="row">
+      <div class="col">{{ name }}</div>
+      <div class="col">{{ body }}</div>
+      <div class="col">{{ user }}</div>
+      <div class="col">edit</div>
+      <div class="col">delete</div>
+      <div class="col">show</div>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-//
+import { api } from 'src/boot/axios'
+import { onMounted, ref } from 'vue'
+
+const name = ref('')
+const body = ref('')
+const user = ref('')
+
+onMounted(() => [
+  api
+    .get('category')
+    .then((r) => {
+      name.value = r.data.name
+      body.value = r.data.body
+      user.value = r.data.name
+    })
+    .catch((e) => {
+      console.log(e)
+    }),
+])
 </script>
