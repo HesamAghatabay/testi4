3daaf17064ba7acbdda1eb52f7eccb7c6455fba4
Hesam Aghatabay <program.h.p.2023@gmail.com>
2025-04-12 16:06:11 +0330

    update

--- FE/src/pages/PostsPage.vue
+++ FE/src/pages/PostsPage.vue
@@ -2,7 +2,7 @@
   <q-page padding>
     <!-- content -->
     <h4>PostsPage</h4>
-    <div class="col-3 q-gutter-lg" v-for="(post, index) in posts" :key="'post' + index">
+    <div class="col-3 q-gutter-lg">
       <q-card>
         <q-card-section>
           <h4>{{ post?.name || 'بدون نام' }}</h4>
@@ -14,14 +14,13 @@
           <q-btn
             color="amber-9"
             @click="
-              postdata.currentPostIndex = index
+              postdata.currentPostIndex = index;
               $router.push('edit-post/' + post.id)
             "
           >
             Edit
           </q-btn>
           <q-btn color="red-9" @click="deletepost(post.id)">Delete</q-btn>
-          <q-btn color="info" @click="$router.push('show-post/' + post.id)">Show</q-btn>
         </q-card-action>
       </q-card>
     </div>
@@ -29,11 +28,13 @@
 </template>
 
 <script setup>
+import { Notify } from 'quasar'
 import { api } from 'src/boot/axios'
 import { onMounted, reactive } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
 const route = useRoute()
+const router = useRouter()
 const post = reactive({
   name: null,
   category: null,
@@ -52,4 +53,24 @@ onMounted(() => {
       console.log(e)
     })
 })
+function deletepost(id) {
+  api
+    .delete(`api/post/${id}`)
+    .then((r) => {
+      Notify.create({
+        type: 'positive',
+        position: 'top',
+        message: 'delete post successful ' + r.data.message,
+      })
+      router.push('discover-post')
+    })
+    .catch((e) => {
+      console.log(e)
+      Notify.create({
+        type: 'negative',
+        position: 'top',
+        message: 'delete post in cach ' + e,
+      })
+    })
+}
 </script>
